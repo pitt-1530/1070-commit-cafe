82dd0ea8c5792ad24a8e2c99801542f2a5b2da18
Kwhites302 <kwhites302@gmail.com>
2025-11-18 11:11:29 -0500

    Change print statement

--- CommitCafe.java
+++ CommitCafe.java
@@ -4,6 +4,6 @@ public class CommitCafe {
         int cups = 0;
 
         // TODO: Adapt this print statement!
-        System.out.println(barista + " brewed " + cups + " cups today.");
+        System.out.println(barista + "!!!!!brewed~~~~" + cups + "~~~~~cups today.~~~~");
     }
 }
